ad7250bdf83113480ffe30ebee7c9ec656400fa4
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-09 06:34:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(count)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(count)
+  while count > 0
+    countdown = "#{count} SECOND(S)!"
+    puts countdown
+    sleep(1)
+    count -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
